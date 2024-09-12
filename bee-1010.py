329a7846477642a29cf51e3d52670b86c1c9f4a1
SanimYousuf <sanimyousuf02@gmail.com>
2024-09-12 11:36:20 +0600

    solved

---
+++ bee-1010.py
@@ -0,0 +1,15 @@
+a,b,c = input().split()
+
+a = int(a)
+b = int(b)
+c = float(c)
+
+d,e,f = input().split()
+
+d = int(d)
+e = int(e)
+f = float(f)
+
+r = (b*c) + (e*f)
+
+print(f"VALOR A PAGAR: R$ {r:.2f}")
