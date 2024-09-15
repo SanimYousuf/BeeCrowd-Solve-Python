4062d7c4bc19c3fe26eb03429d5c68d6cc13482e
SanimYousuf <sanimyousuf02@gmail.com>
2024-09-15 19:32:42 +0600

    solved

--- bee-1047.py
+++ bee-1047.py
@@ -1,14 +1,23 @@
 a, b, c, d = map(int, input().split())
 
-if b > d:
-    d += 60
-    a -= 1
+con = [
+    b==d,
+    a==c,
+    (a==c) != (b==d)
+]
+if all(con):
+    print("O JOGO DUROU 0 HORA(S) E 0 MINUTO(S)")
+else:
+    if b > d:
+        d += 60
+        c -= 1
     m = d - b
 
     if a > c:
         c += 24
-        h = c - a
-        print(f"O JOGO DUROU {h} HORA(S) E {m} MINUTO(S)")
+    h = c - a
+
+    if h == 0 and m==0:
+        print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
     else:
-        h = c - a
-        print(f"O JOGO DUROU {h} HORA(S) E {m} MINUTO(S)")
+        print(f"O JOGO DUROU {h} HORA(S) E {m} MINUTO(S)")  
